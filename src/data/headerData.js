b751a45504eed873041550e7336dc371ab4d8d04
samuelmoiwa <samuelmoiwa@gmail.com>
2023-04-22 19:43:25 +0000

    update image files

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,4 +1,5 @@
-
+import IMG_1 from '../images/IMG_1.jpg'
+import IMG_2 from '../images/IMG_2.jpg'
 export const headerData = {
     name: 'Abdulai Samuel Moiwa',
     title: "Hello! I am",
@@ -7,7 +8,7 @@ export const headerData = {
     one of my biggest passions, and I take pride in my ability to find creative solutions to complex
     challenges. With my self-learning attitude, I am always looking for opportunities to grow and
     improve as a programmer `,
-    image: 'https://github.com/samuelmoiwa/moiwa-portfolio/blob/develop/images/IMG_1834.jpg',
-    imagebw: 'https://github.com/samuelmoiwa/moiwa-portfolio/blob/develop/images/IMG_20230422_191031.jpg',
+    image: IMG_2,
+    imagebw: IMG_1,
     resumePdf: 'https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing'
 }
