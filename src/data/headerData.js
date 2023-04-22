d59dd067233340b45c74748d34d50986fe1089dd
samuelmoiwa <samuelmoiwa@gmail.com>
2023-04-22 19:14:27 +0000

    update files

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,8 +1,12 @@
 
 export const headerData = {
-    name: 'ABU SAID',
+    name: 'Abdulai Samuel Moiwa',
     title: "Hello! I am",
-    desciption: "Hello! I am ABU SAID, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
+    desciption: `Hello there, I'm Abdulai Samue Moiwa - a passionate and professional programmer.
+    I have a strong appetite for learning and enjoy exploring new technologies. Problem-solving is
+    one of my biggest passions, and I take pride in my ability to find creative solutions to complex
+    challenges. With my self-learning attitude, I am always looking for opportunities to grow and
+    improve as a programmer `,
     image: 'https://i.ibb.co/ZmdNH6x/abu-said.jpg',
     imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
     resumePdf: 'https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing'
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './app';
 import ThemeContextProvider from './contexts/theme-context';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+
 const theme = createTheme();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <ThemeContextProvider>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </ThemeContextProvider>
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeContextProvider>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ThemeContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 reportWebVitals();
