d20d187235763fbb7960c6b77ae1a0dccd124f7c
samuelmoiwa <samuelmoiwa@gmail.com>
2023-04-22 19:17:45 +0000

    update files

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -7,7 +7,7 @@ export const headerData = {
     one of my biggest passions, and I take pride in my ability to find creative solutions to complex
     challenges. With my self-learning attitude, I am always looking for opportunities to grow and
     improve as a programmer `,
-    image: 'https://i.ibb.co/ZmdNH6x/abu-said.jpg',
-    imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
+    image: 'https://github.com/samuelmoiwa/moiwa-portfolio/blob/develop/images/IMG_1834.jpg',
+    imagebw: 'https://github.com/samuelmoiwa/moiwa-portfolio/blob/develop/images/IMG_20230422_191031.jpg',
     resumePdf: 'https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing'
 }
